c886ba87f1251c964acce66f4f3a5cba2fb018ce
Julio MS <saitodisse@gmail.com>
2025-05-29 12:37:59 -0300

    feat: enhance mergeTracks tests with additional cases and type definitions

--- app/discogs/masters/[master_id]/save/actions.test.ts
+++ app/discogs/masters/[master_id]/save/actions.test.ts
@@ -1,68 +1,231 @@
-import { ReleaseDb } from "@/types/ReleaseDb";
-import { mergeTracks } from "./actions";
+import { ReleaseDb } from '@/types/ReleaseDb'
+import { mergeTracks } from './actions'
+import { Track } from 'disconnect'
 
-describe("mergeTracks", () => {
-    it("should return empty array when both inputs are undefined", () => {
-        expect(mergeTracks(undefined, undefined)).toEqual([]);
-    });
+describe('mergeTracks', () => {
+  it('should return empty array when both inputs are undefined', () => {
+    expect(mergeTracks(undefined, undefined)).toEqual([])
+  })
 
-    it("should return updatedTracks when originalTracks is undefined", () => {
-        const updatedTracks = [{ title: "Track 1" }];
-        expect(mergeTracks(undefined, updatedTracks)).toEqual(updatedTracks);
-    });
+  it('should return updatedTracks when originalTracks is undefined', () => {
+    const updatedTracks: Track[] = [
+      {
+        position: 'A1',
+        type_: 'track',
+        title: 'Dá Sorte',
+        duration: '',
+      },
+    ]
+    expect(mergeTracks(undefined, updatedTracks)).toEqual(updatedTracks)
+  })
 
-    it("should return originalTracks when updatedTracks is undefined", () => {
-        const originalTracks = [{ title: "Track 1" }];
-        expect(mergeTracks(originalTracks, undefined)).toEqual(originalTracks);
-    });
+  it('should return originalTracks when updatedTracks is undefined', () => {
+    const originalTracks: Track[] = [
+      {
+        position: 'A1',
+        type_: 'track',
+        title: 'Dá Sorte',
+        duration: '',
+      },
+    ]
+    expect(mergeTracks(originalTracks, undefined)).toEqual(originalTracks)
+  })
 
-    it("should merge tracks and mark new tracks as extra_track", () => {
-        const originalTracks = [
-            { title: "Track 1", extraartists: [] },
-            { title: "Track 2", extraartists: [] },
-        ];
-        const updatedTracks = [
-            { title: "Track 2", extraartists: [] },
-            { title: "Track 3", extraartists: [] },
-        ];
+  it('should merge tracks and mark new tracks as extra_track', () => {
+    const originalTracks: Track[] = [
+      {
+        position: 'A1',
+        type_: 'track',
+        title: 'Track 1',
+        duration: '',
+        extraartists: [],
+      },
+      {
+        position: 'A2',
+        type_: 'track',
+        title: 'Track 2',
+        duration: '',
+        extraartists: [],
+      },
+    ]
+    const updatedTracks: Track[] = [
+      {
+        position: 'A2',
+        type_: 'track',
+        title: 'Track 2',
+        duration: '',
+        extraartists: [],
+      },
+      {
+        position: 'A3',
+        type_: 'track',
+        title: 'Track 3',
+        duration: '',
+        extraartists: [],
+      },
+    ]
 
-        const expected = [
-            { title: "Track 1", extraartists: [] },
-            { title: "Track 2", extraartists: [] },
-            { title: "Track 3", extraartists: [], extra_track: true },
-        ];
+    const expected: Track[] = [
+      {
+        position: 'A1',
+        type_: 'track',
+        title: 'Track 1',
+        duration: '',
+        extraartists: [],
+      },
+      {
+        position: 'A2',
+        type_: 'track',
+        title: 'Track 2',
+        duration: '',
+        extraartists: [],
+      },
+      {
+        position: 'A3',
+        type_: 'track',
+        title: 'Track 3',
+        duration: '',
+        extraartists: [],
+        extra_track: true,
+      },
+    ]
 
-        expect(mergeTracks(originalTracks, updatedTracks)).toEqual(expected);
-    });
+    expect(mergeTracks(originalTracks, updatedTracks)).toEqual(expected)
+  })
 
-    it("should update existing track if updated track has more extra artists", () => {
-        const originalTracks = [
-            { title: "Track 1", extraartists: [] },
-        ];
-        const updatedTracks = [
-            { title: "Track 1", extraartists: [{ name: "Artist 1" }] },
-        ];
+  it('should not merge tracks if only changes title', () => {
+    const originalTracks: Track[] = [
+      {
+        position: 'A2',
+        type_: 'track',
+        title: 'Track 2',
+        duration: '',
+        extraartists: [],
+      },
+    ]
+    const updatedTracks: Track[] = [
+      {
+        position: 'A2',
+        type_: 'track',
+        title: 'Track 2 (OTHER NAME)',
+        duration: '',
+        extraartists: [],
+      },
+    ]
 
-        const expected = [
-            { title: "Track 1", extraartists: [{ name: "Artist 1" }] },
-        ];
+    const expected: Track[] = [
+      {
+        position: 'A2',
+        type_: 'track',
+        title: 'Track 2',
+        duration: '',
+        extraartists: [],
+      },
+    ]
 
-        expect(mergeTracks(originalTracks, updatedTracks)).toEqual(expected);
-    });
+    expect(mergeTracks(originalTracks, updatedTracks)).toEqual(expected)
+  })
 
-    it("should keep original track if it has more or equal extra artists", () => {
-        const originalTracks = [
-            {
-                title: "Track 1",
-                extraartists: [{ name: "Artist 1" }, { name: "Artist 2" }],
-            },
-        ];
-        const updatedTracks = [
-            { title: "Track 1", extraartists: [{ name: "Artist 3" }] },
-        ];
+  it('should update existing track if updated track has more extra artists', () => {
+    const originalTracks: Track[] = [
+      {
+        position: 'A1',
+        type_: 'track',
+        title: 'Track 1',
+        duration: '',
+        extraartists: [],
+      },
+    ]
+    const updatedTracks: Track[] = [
+      {
+        position: 'A1',
+        type_: 'track',
+        title: 'Track 1',
+        duration: '',
+        extraartists: [
+          {
+            name: 'Carlos Imperial',
+            anv: '',
+            join: '',
+            role: 'Composed By',
+            tracks: '',
+            id: 736513,
+            resource_url: 'https://api.discogs.com/artists/736513',
+          },
+        ],
+      },
+    ]
 
-        expect(mergeTracks(originalTracks, updatedTracks)).toEqual(
-            originalTracks,
-        );
-    });
-});
+    const expected = [
+      {
+        position: 'A1',
+        type_: 'track',
+        title: 'Track 1',
+        duration: '',
+        extraartists: [
+          {
+            name: 'Carlos Imperial',
+            anv: '',
+            join: '',
+            role: 'Composed By',
+            tracks: '',
+            id: 736513,
+            resource_url: 'https://api.discogs.com/artists/736513',
+          },
+        ],
+      },
+    ]
+
+    expect(mergeTracks(originalTracks, updatedTracks)).toEqual(expected)
+  })
+
+  it('should keep original track if it has more or equal extra artists', () => {
+    const originalTracks: Track[] = [
+      {
+        position: 'A1',
+        type_: 'track',
+        title: 'Track 1',
+        extraartists: [
+          {
+            name: 'Carlos Imperial',
+            anv: '',
+            join: '',
+            role: 'Composed By',
+            tracks: '',
+            id: 736513,
+            resource_url: 'https://api.discogs.com/artists/736513',
+          },
+          {
+            name: 'Elis Regina',
+            anv: '',
+            join: '',
+            role: 'Vocals',
+            tracks: '',
+            id: 30703,
+            resource_url: 'https://api.discogs.com/artists/30703',
+          },
+        ],
+      },
+    ]
+    const updatedTracks: Track[] = [
+      {
+        position: 'A1',
+        type_: 'track',
+        title: 'Track 1',
+        extraartists: [
+          {
+            name: 'Elis Regina',
+            anv: '',
+            join: '',
+            role: 'Vocals',
+            tracks: '',
+            id: 30703,
+            resource_url: 'https://api.discogs.com/artists/30703',
+          },
+        ],
+      },
+    ]
+
+    expect(mergeTracks(originalTracks, updatedTracks)).toEqual(originalTracks)
+  })
+})
